71ac7d860106f19596be14c59f3ac889830923c0
John Alex <jpalex@gmail.com>
2026-08-01 06:58:01 -0400

    Add automated TOML diffing unit tests

---
+++ CIME/tests/test_unit_compare_namelists.py
@@ -0,0 +1,62 @@
+import unittest
+import tempfile
+import os
+
+from CIME.compare_namelists import compare_namelist_files, is_namelist_file
+
+class TestCompareNamelists(unittest.TestCase):
+    def test_toml_whitespace_torture(self):
+        gold_toml = """
+[route_opt]
+value = 5
+# a comment
+"""
+        compare_toml = """
+   [ route_opt ]    
+        value      =       5
+"""
+        with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".toml") as f1, \
+             tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".toml") as f2:
+            f1.write(gold_toml)
+            f2.write(compare_toml)
+            f1_name = f1.name
+            f2_name = f2.name
+
+        try:
+            self.assertTrue(is_namelist_file(f1_name))
+            match, comments = compare_namelist_files(f1_name, f2_name)
+            self.assertTrue(match)
+            self.assertEqual(comments, "")
+        finally:
+            os.remove(f1_name)
+            os.remove(f2_name)
+
+    def test_toml_compare_diff(self):
+        gold_toml = """
+[route_opt]
+value = 5
+
+[physics]
+method = "IRF"
+"""
+        compare_toml = """
+[route_opt]
+value = 5
+
+[physics]
+method = "MC"
+"""
+        with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".toml") as f1, \
+             tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".toml") as f2:
+            f1.write(gold_toml)
+            f2.write(compare_toml)
+            f1_name = f1.name
+            f2_name = f2.name
+
+        try:
+            match, comments = compare_namelist_files(f1_name, f2_name)
+            self.assertFalse(match)
+            self.assertIn("method had mismatched values", comments)
+        finally:
+            os.remove(f1_name)
+            os.remove(f2_name)
